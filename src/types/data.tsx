9717b7609b7e028d7cbe3698f41091c751f65233
mohammadreza <mohammadrezaaataeii@gmail.com>
2026-01-30 23:49:51 +0330

    dommy data added

---
+++ src/types/data.tsx
@@ -0,0 +1,36 @@
+import type { ComponentType, SVGProps } from "react";
+
+interface BoxIconProps extends SVGProps<SVGSVGElement> {
+  pack?: "basic" | "filled" | "brands";
+  fill?: string;
+  opacity?: number | string;
+  width?: number | string;
+  height?: number | string;
+  size?:
+    | "xs"
+    | "sm"
+    | "base"
+    | "md"
+    | "lg"
+    | "xl"
+    | "2xl"
+    | "3xl"
+    | "4xl"
+    | "5xl";
+  flip?: "horizontal" | "vertical";
+  rotate?: number | string;
+  removePadding?: boolean;
+}
+
+export interface navItemsType {
+  label: string;
+  icon: ComponentType<BoxIconProps>;
+  activeIcon: ComponentType<BoxIconProps>;
+  inactiveIcon: ComponentType<BoxIconProps>;
+}
+
+export interface socialType {
+  label: string;
+  icon: ComponentType<BoxIconProps>;
+  url: string;
+}
