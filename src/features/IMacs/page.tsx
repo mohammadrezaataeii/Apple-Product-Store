e3ef048c808caa315db1d8b57d1e7468eac22fda
mohammadreza <mohammadrezaaataeii@gmail.com>
2026-02-17 01:29:17 +0330

    i mac added

---
+++ src/features/IMacs/page.tsx
@@ -0,0 +1,63 @@
+import { imacModels as iMacModels } from "@/data/page";
+import { useState } from "react";
+
+export default function IMacs() {
+  const [selectedColor, setSelectedColor] = useState<number>(0);
+  const [visibleLabel, setVisibleLabel] = useState<string>("Green");
+  const currentIMac = iMacModels[selectedColor];
+
+  return (
+    <div className="h-full  xl:w-260 md:w-170 grid grid-cols-2 grid-rows-2 md:gap-4 gap-1 bg-white relative">
+      <div className="h-full col-span-1 row-span-2 flex flex-col items-center md:justify-center justify-start pt-8 bg-gray-50">
+        <img
+          src={currentIMac.images.side}
+          alt="IMac"
+          className="2xl:max-w-full xl:max-w-36 max-w-24"
+        />
+      </div>
+      <div className="bg-gray-50 col-span-1 row-span-1 flex items-center justify-center p-4">
+        <img
+          src={currentIMac.images.front}
+          alt="IMac"
+          className="2xl:max-w-full xl:max-w-52 lg:max-w-48"
+        />
+      </div>
+      <div className="bg-gray-50 col-span-1 row-span-1 flex items-center justify-center p-4">
+        <img
+          src={currentIMac.images.back}
+          alt="IMac"
+          className="2xl:max-w-full xl:max-w-52 lg:max-w-48"
+        />
+      </div>
+      <div className="absolute md:top-1/2 top-3/4 md:left-4 left-12 transform -translate-y-1/2 flex flex-col lg:space-y-2 space-y-1 bg-gray-50">
+        {iMacModels.map((model, index) => (
+          <div
+            key={index}
+            className="flex items-center space-x-2"
+            onClick={() => {
+              setSelectedColor(index);
+              setVisibleLabel(model.label);
+            }}
+          >
+            <button
+              style={{ backgroundColor: model.bg }}
+              className={`2xl:w-8 md:w-5 w-4 2xl:h-8 md:h-5 h-4 rounded-full flex items-center justify-center `}
+            />
+            {visibleLabel === model.label && (
+              <span className="text-sm" style={{ color: model.bg }}>
+                {model.label}
+              </span>
+            )}
+          </div>
+        ))}
+
+        <button
+          type="button"
+          className="text-sm mt-4 w-min py-1 px-2 bg-blue-400 rounded-full text-white"
+        >
+          Shop
+        </button>
+      </div>
+    </div>
+  );
+}
