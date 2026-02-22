231d2ce4bf178e9458138ad43b08661e15dce0d0
mohammadreza <mohammadrezaaataeii@gmail.com>
2026-02-23 00:16:42 +0330

    handle table size

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Controls from "./features/app/components/Controls";
 import Navbar from "./features/app/components/Navbar";
 import AppContainer from "./features/app/components/AppContainer";
 import Home from "./features/home/page";
 import Iphones from "./features/iphones/page";
 import MacBooks from "./features/macbook/page";
+import Watches from "./features/watches/page";
+import IMacs from "./features/IMacs/page";
+import PageTransition from "./components/PageTransition";
 
 export default function App() {
   const [frameZoom, setFrameZoom] = useState<boolean>(false);
   const [activePage, setActivePage] = useState<number>(0);
+  const [isLgScreen, setIsLgScreen] = useState<boolean>(
+    window.innerWidth > 1024,
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLgScreen(window.innerWidth >= 1024);
+      if (window.innerWidth < 1024) {
+        setFrameZoom(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const toggleZoom = () => {
-    setFrameZoom(!frameZoom);
+    if (isLgScreen) {
+      setFrameZoom(!frameZoom);
+    }
   };
 
   const handleNavClick = (index: number) => {
     setActivePage(index);
   };
 
+  const resetPage = () => {
+    setActivePage(0);
+  };
+
   return (
     <AppContainer frameZoom={frameZoom}>
       <Navbar activePage={activePage} handleNavClick={handleNavClick} />
-      <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
-      {/* <Home handleNavClick={handleNavClick} /> */}
-      {/* <Iphones /> */}
-      <MacBooks />
+      <Controls
+        toggleZoom={toggleZoom}
+        resetPage={resetPage}
+        frameZoom={frameZoom}
+        activePage={activePage}
+      />
+      <PageTransition activePage={activePage}>
+        <Home handleNavClick={handleNavClick} />
+        <Iphones />
+        <MacBooks />
+        <Watches />
+        <IMacs />
+      </PageTransition>
     </AppContainer>
   );
 }
--- src/features/app/components/Controls.tsx
+++ src/features/app/components/Controls.tsx
@@ -3,14 +3,21 @@ import { SearchBigMinus, SearchBigPlus, XCircle } from "@boxicons/react";
 interface ControlProps {
   toggleZoom: () => void;
   frameZoom: boolean;
+  resetPage: () => void;
+  activePage: number;
 }
 
-function Controls({ toggleZoom, frameZoom }: ControlProps) {
+function Controls({
+  toggleZoom,
+  frameZoom,
+  resetPage,
+  activePage,
+}: ControlProps) {
   return (
-    <div className="absolute top-3 right-3 space-x-2">
+    <div className="absolute top-3 right-3 space-x-2 z-10">
       <button
         onClick={toggleZoom}
-        className="text-2xl text-pink-400 cursor-pointer"
+        className="text-2xl text-pink-400 cursor-pointer lg:inline-block"
       >
         {frameZoom ? (
           <SearchBigMinus pack="filled" size="base" />
@@ -18,7 +25,10 @@ function Controls({ toggleZoom, frameZoom }: ControlProps) {
           <SearchBigPlus pack="filled" size="base" />
         )}
       </button>
-      <button className="text-2xl text-pink-400 cursor-pointer">
+      <button
+        className={`${activePage === 0 ? " text-pink-200 cursor-not-allowed" : "text-pink-400 cursor-pointer"} text-2xl`}
+        onClick={() => resetPage()}
+      >
         <XCircle size="base" />
       </button>
     </div>
